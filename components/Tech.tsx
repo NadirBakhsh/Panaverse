b1051f861c09faaffbcf932f89d18fdec8ae05e9
NadirBakhsh <nadir4k2010@gmail.com>
2023-04-15 02:02:13 +0500

    tech completed

--- components/Tech.tsx
+++ components/Tech.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+"use client"
+import { BallCanvas } from "./canvas"
+import { SectionWrapper } from "@/hoc" 
+import { technologies } from "@/constants"
+import uuid from "react-uuid"
+
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className="flex flex-row flex-wrap gap-10 justify-center">
+      {
+        technologies.map((technology) => (
+          <>
+          <div className="w-28 h-28" key={uuid()}>
+            {technology.icon && <BallCanvas icon={technology.icon} />}
+          </div>
+          </>
+        ))
+      }
+    </div>
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech, '')
